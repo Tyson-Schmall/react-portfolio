7bae7f8dda20ed8c8ca43dca4be36ad145b704e7
Tyson-Schmall <tyson.schmall@gmail.com>
2020-04-02 09:32:38 -0600

    Custom functions in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -7,12 +7,20 @@ export default class PortfolioContainer extends Component {
         super();
     }
 
+    portfolioItems() {
+        const data = ["Quip", "Eventbrite", "Ministry Safe"];
+
+        return data.map(item => {
+            return <PortfolioItem />;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here...</h2>
 
-                <PortfolioItem />            
+                {this.portfolioItems()}  
             </div>
 
         )
