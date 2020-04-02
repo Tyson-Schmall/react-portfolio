a62d198a6bdac714b3747990f8d9e022118ca6a1
Tyson-Schmall <tyson.schmall@gmail.com>
2020-04-02 11:01:56 -0600

    Introduction to State in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,20 +5,28 @@ import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                {title: "Quip" },
+                {title: "Eventbrite" }, 
+                {title: "Ministry Safe" },
+                {title: "SwingAway" }
+            ]
+        };
     }
 
     portfolioItems() {
-        const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"];
-
         return data.map(item => {
-            return <PortfolioItem title={item} url="https://www.google.com"/>;
+            return <PortfolioItem title={item.title} url="https://www.google.com"/>;
         })
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}  
             </div>
