7f54d4ab28daafc31f9f1b6935954f6548f95fe8
Tyson-Schmall <tyson.schmall@gmail.com>
2020-04-07 14:04:25 -0600

    added in categories to the data colection in my constructor, added buttons in for each category with handleFilter

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,16 +9,26 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio",
             data: [
-                {title: "Quip" },
-                {title: "Eventbrite" }, 
-                {title: "Ministry Safe" },
-                {title: "SwingAway" }
+                {title: "Quip", category: "eCommerce" },
+                {title: "Eventbrite", category: "Scheduling"  }, 
+                {title: "Ministry Safe", category: "Enterprise"  },
+                {title: "SwingAway", category: "eCommerce"  }
             ]
         };
+
+        this.handleFilter = this.handleFilter.bind(this)
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        });
     }
 
     portfolioItems() {
-        return data.map(item => {
+        return this.state.data.map(item => {
             return <PortfolioItem title={item.title} url="https://www.google.com"/>;
         })
     }
@@ -28,9 +38,20 @@ export default class PortfolioContainer extends Component {
             <div>
                 <h2>{this.state.pageTitle}</h2>
 
+                <button onClick={() => this.handleFilter('eCommerce')}>
+                    eCommerce
+                </button>
+
+                <button onClick={() => this.handleFilter('Scheduling')}>
+                    Scheduling
+                </button>
+
+                <button onClick={() => this.handleFilter('Enterprise')}>
+                    Enterprise
+                </button>
+
                 {this.portfolioItems()}  
             </div>
-
-        )
+        );
     }
 }
