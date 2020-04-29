46f9a783091ea09daa926e272e9e685975ea6c67
Tyson-Schmall <tyson.schmall@gmail.com>
2020-04-28 20:08:10 -0600

    Finalized behaviors for editing blog posts and specific parts of blog posts.

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -12,7 +12,10 @@ export default class BlogForm extends Component {
       id: "",
       title: "",
       blog_status: "",
+      content: "",
       featured_image: "",
+      apiUrl: "https://tysonschmall.devcamp.space/portfolio/portfolio_blogs",
+      apiAction: "post",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -48,7 +51,10 @@ export default class BlogForm extends Component {
       this.setState({
         id: this.props.blog.id,
         title: this.props.blog.title,
-        status: this.props.blog.status,
+        blog_status: this.props.blog.blog_status,
+        content: this.props.blog.content,
+        apiUrl: `https://tysonschmall.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+        apiAction: "patch",
       });
     }
   }
@@ -96,12 +102,12 @@ export default class BlogForm extends Component {
   }
 
   handleSubmit(event) {
-    axios
-      .post(
-        "https://tysonschmall.devcamp.space/portfolio/portfolio_blogs",
-        this.buildForm(),
-        { withCredentials: true }
-      )
+    axios({
+      method: this.state.apiAction,
+      url: this.state.apiUrl,
+      data: this.buildForm(),
+      withCredentials: true,
+    })
       .then((response) => {
         if (this.state.featured_image) {
           this.featuredImageRef.current.dropzone.removeAllFiles();
@@ -113,6 +119,15 @@ export default class BlogForm extends Component {
           content: "",
           featured_image: "",
         });
+
+        if (this.props.editMode) {
+          // Update blog detail
+          this.props.handleUpdateFormSubmission(response.data.portfolio_blog);
+        } else {
+          this.props.handleSuccessfulFormSubmission(
+            response.data.portfolio_blog
+          );
+        }
 
         this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
       })
